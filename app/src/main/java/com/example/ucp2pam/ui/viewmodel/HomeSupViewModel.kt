2059a53d0aaf5e3838b0cc245c59fac39245a6f3
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2024-12-24 03:37:16 +0700

    Menambahkan HomeSupViewModel untuk mengelola state UI data Suplier

---
+++ app/src/main/java/com/example/ucp2pam/ui/viewmodel/HomeSupViewModel.kt
@@ -0,0 +1,54 @@
+package com.example.ucp2pam.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2pam.data.entity.Suplier
+import com.example.ucp2pam.repository.RepositorySuplier
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class HomeSupViewModel(
+    private val repositorySpl: RepositorySuplier
+): ViewModel(){
+    val homeSplUiState: StateFlow<HomeSplUiState> = repositorySpl.getAllSpl()
+        .filterNotNull()
+        .map {
+            HomeSplUiState(
+                listSpl = it.toList(),
+                isLoading = false
+            )
+        }
+        .onStart {
+            emit(HomeSplUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeSplUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeSplUiState(
+                isLoading = true
+            )
+        )
+}
+
+data class HomeSplUiState(
+    val listSpl: List<Suplier> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
