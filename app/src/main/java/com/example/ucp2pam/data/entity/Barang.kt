5464fd205d0100247c6a644c3642b31def019227
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2024-12-24 03:15:17 +0700

    Menambahkan entity Barang dengan relasi ke tabel Suplier

--- app/src/main/java/com/example/ucp2pam/data/entity/Barang.kt
+++ app/src/main/java/com/example/ucp2pam/data/entity/Barang.kt
@@ -1,16 +1,28 @@
 package com.example.ucp2pam.data.entity
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
 
-@Entity(tableName= "barang")
+@Entity(
+    tableName = "barang",
+    foreignKeys = [
+        ForeignKey(
+            entity = Suplier::class,
+            parentColumns = ["nama"],
+            childColumns = ["namaSuplier"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class Barang(
-    @PrimaryKey
-    val id: String,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
     val nama: String,
     val deskripsi: String,
     val harga: String,
     val stok: String,
     val namaSuplier: String,
 )
+
